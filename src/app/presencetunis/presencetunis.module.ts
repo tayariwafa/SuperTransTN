218966abfbe1513dc1fe6d080d1d88201a42f9a3
tayariwafa <tayariwafa6@gmail.com>
2020-03-24 04:46:03 +0100

    second commit

--- src/app/presencetunis/presencetunis.module.ts
+++ src/app/presencetunis/presencetunis.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { PresencetunisRoutingModule } from './presencetunis-routing.module';
 import { PresencetunisComponent } from './presencetunis.component';
+import { SharedModule } from '../theme/shared/shared.module';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [PresencetunisComponent],
   imports: [
     CommonModule,
-    PresencetunisRoutingModule
+    PresencetunisRoutingModule,
+    NgbDropdownModule,
+    SharedModule
   ]
 })
 export class PresencetunisModule { }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { SharedModule } from '../theme/shared/shared.module';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [ProfileComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    SharedModule,
+    NgbDropdownModule
   ]
 })
 export class ProfileModule { }
